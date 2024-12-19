55ae790a4a0ae575e3e8b982c75be7818da3167c
Mattegbg <90768274+Mattegbg@users.noreply.github.com>
2024-12-19 20:53:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Serve static files (e.g., index.html)
-app.use(express.static(dirname));
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-    res.sendFile(dirname + "/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(port, () => {
-    console.log(Server is running on port ${port});
+    console.log('Server is running on port ${port}')
 });
